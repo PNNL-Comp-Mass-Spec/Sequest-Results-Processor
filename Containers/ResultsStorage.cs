47e23864f8d890133a3662985fc12ab677879fe2
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-07 20:46:35 -0700

    Check for null

--- Containers/ResultsStorage.cs
+++ Containers/ResultsStorage.cs
@@ -263,6 +263,15 @@ namespace SequestResultsProcessor.Containers
         {
             public int Compare(OutputRecordIndex x, OutputRecordIndex y)
             {
+                if (x == null && y == null)
+                    return 0;
+
+                if (x == null)
+                    return -1;
+
+                if (y == null)
+                    return 1;
+
                 if (x.XCorr > y.XCorr)
                 {
                     return -1;
